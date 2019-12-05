44d2c470e21ce9492a85cafe74e2eaa8ed22f9cf
gl19 <gl19@illinois.edu>
2019-12-05 02:05:35 -0600

    0.0.3

--- app/src/main/java/com/example/geotrending/MainActivity.java
+++ app/src/main/java/com/example/geotrending/MainActivity.java
@@ -5,12 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Status;
 import twitter4j.Twitter;
+import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.GeoTrending.MESSAGE";
@@ -19,13 +25,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Twitter twitter = TwitterFactory.getSingleton();
+        //Twitter O-Auth
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey("9alVPGLKVIp0sEtoa1b3tZEnn")
+                .setOAuthConsumerSecret("0y8YVdeU09laY402BPJJNqXeWxOmdosJoZn5BPDZgH1Yft2SWP")
+                .setOAuthAccessToken("1199152818038308864-egUurjskATwRwHzf5CcppBuAtQGjPQ")
+                .setOAuthAccessTokenSecret("JiTLQX3RcVEsZ4JBYeorKYJfZQNta5B0vRvq5EGoShUcA");
 
+        TwitterFactory tf = new TwitterFactory(cb.build());
+        Twitter twitter = tf.getInstance();
+        Query query = new Query("source:twitter4j yusukey");
+        QueryResult result = null;
+        try {
+            result = twitter.search(query);
+        } catch (TwitterException e) {
+            e.printStackTrace();
+        }
+        for (Status status : result.getTweets()) {
+            Log.v("nani","@" + status.getUser().getScreenName() + ":" + status.getText());
+        }
     }
 
     public void searchLocation(View view) {
         Intent intent = new Intent(this, TopTenTrending.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
+        EditText editText = (EditText) findViewById(R.id.locationSearch);
         String message = editText.getText().toString();
 
         // Empty search results in no action and toast is shown to user
